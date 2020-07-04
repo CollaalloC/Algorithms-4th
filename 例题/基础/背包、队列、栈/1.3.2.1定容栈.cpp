0255a9c0addc71958838ea0ab6dccc62a31dbf05
CollaalloC <1812315571@qq.com>
2020-07-04 18:12:44 +0800

    建类

---
+++ 例题/基础/背包、队列、栈/1.3.2.1定容栈.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class FixedCapacityStackOfStrings
+{
+private:
+    string s;
+public:
+    FixedCapacityStackOfStrings(/* args */);
+    ~FixedCapacityStackOfStrings();
+};
+
+FixedCapacityStackOfStrings::FixedCapacityStackOfStrings(/* args */)
+{
+}
+
+FixedCapacityStackOfStrings::~FixedCapacityStackOfStrings()
+{
+}
+
+
+
+
