06870a9fcb94085aa9a5525e5a8af71604168f28
CollaalloC <1812315571@qq.com>
2020-07-11 23:32:28 +0800

    第一种 定容栈

--- 例题/基础/背包、队列、栈/1.3.2.1定容栈.cpp
+++ 例题/基础/背包、队列、栈/1.3.2.1定容栈.cpp
@@ -1,25 +1,31 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class FixedCapacityStackOfStrings
 {
 private:
-    string s;
+    int size;
+    string *s;
+
 public:
-    FixedCapacityStackOfStrings(/* args */);
+    FixedCapacityStackOfStrings(int n);
     ~FixedCapacityStackOfStrings();
+    int get_size() { return size; }
+    void push(string item) { s[size++] = item; }
+    string pop() { return s[--size]; }
+    bool isEmpty() { return get_size() == 0; }
 };
 
-FixedCapacityStackOfStrings::FixedCapacityStackOfStrings(/* args */)
+FixedCapacityStackOfStrings::FixedCapacityStackOfStrings(int n)
 {
+    size = 0;
+    s = new string[n];
 }
 
 FixedCapacityStackOfStrings::~FixedCapacityStackOfStrings()
 {
+    delete[] s;
 }
-
-
-
-
