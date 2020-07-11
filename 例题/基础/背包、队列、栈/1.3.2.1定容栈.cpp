8c8eca5a94639443d93eda9f94fea54f214e05e8
CollaalloC <1812315571@qq.com>
2020-07-11 23:41:38 +0800

    补充注释

--- 例题/基础/背包、队列、栈/1.3.2.1定容栈.cpp
+++ 例题/基础/背包、队列、栈/1.3.2.1定容栈.cpp
@@ -14,18 +14,18 @@ public:
     FixedCapacityStackOfStrings(int n);
     ~FixedCapacityStackOfStrings();
     int get_size() { return size; }
-    void push(string item) { s[size++] = item; }
-    string pop() { return s[--size]; }
+    void push(string item) { s[size++] = item; }//压栈 但当数量超过容量时会越界，出现内存泄漏错误
+    string pop() { return s[--size]; }//弹出
     bool isEmpty() { return get_size() == 0; }
 };
 
 FixedCapacityStackOfStrings::FixedCapacityStackOfStrings(int n)
 {
-    size = 0;
-    s = new string[n];
+    size = 0;//统计压入栈的数量，并非栈的大小
+    s = new string[n]; //确定数组的大小，确定容量；
 }
 
 FixedCapacityStackOfStrings::~FixedCapacityStackOfStrings()
 {
-    delete[] s;
+    delete[] s; //回收内存，必须要有；
 }
