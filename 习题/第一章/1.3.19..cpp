90dd6295c78e931e828d3288235a62527813a6d3
CollaalloC <1812315571@qq.com>
2020-07-21 12:37:15 +0800

    建立链表

---
+++ 习题/第一章/1.3.19..cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+//删除链表的尾节点
+//遍历法
+//创建链表
+struct node
+{
+    int a;
+    node* next;
+};
+
+//创建一个大小为20的链表
+node* creat(int Array[])
+{
+    node *pre, *p ,*first;
+    first = new node;
+    first->next = NULL;
+    for(int i = 0; i < 20; i++)
+    {
+
+    }
+}
